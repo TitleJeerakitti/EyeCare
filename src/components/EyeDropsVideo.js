66e354fcbc69a99bd13f34b820105b5cb077ce15
Unknown <xclosis.time@hotmail.com>
2019-04-03 22:06:43 +0700

    fix video size

--- src/components/EyeDropsVideo.js
+++ src/components/EyeDropsVideo.js
@@ -1,34 +1,27 @@
 import React from 'react';
-import { View, Dimensions } from 'react-native';
+import { View } from 'react-native';
 import { Video, ScreenOrientation } from 'expo';
 import { NavigationEvents } from "react-navigation";
 
 class VideoPlayer extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            screenWidth: Dimensions.get('window').width,
-            heightScaled: 300,
-        };
-    }
 
     handleVideoRef = component => {
         this.playbackObject = component;
-      }
-      
+    }
+
     render() {
         return (
             <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
                 <NavigationEvents
                     onWillBlur={() => {
-                    this.playbackObject.pauseAsync();
+                        this.playbackObject.pauseAsync();
                     }}
                 />
                 <Video
                     ref={this.handleVideoRef}
                     style={{
-                        width: this.state.screenWidth,
-                        height: this.state.heightScaled
+                        width: '100%',
+                        height: '100%'
                     }}
                     source={require('../videos/EyeDropsVideo.mp4')}
                     rate={1.0}
@@ -38,13 +31,6 @@ class VideoPlayer extends React.Component {
                     shouldPlay
                     isLooping={false}
                     useNativeControls
-                    onReadyForDisplay={event => {
-                        const { width, height } = event.naturalSize;
-
-                        this.setState({
-                            heightScaled: height * (this.state.screenWidth / width),
-                        });
-                    }}
                     onFullscreenUpdate={event => {
                         if (event.fullscreenUpdate === 0) {
                             ScreenOrientation.allowAsync(ScreenOrientation.Orientation.LANDSCAPE);
