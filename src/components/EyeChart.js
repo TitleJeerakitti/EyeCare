b833091f419fc258256d54bb63495f4b099b6695
Rattasat <rattasat.student@gmail.com>
2019-10-03 17:20:31 +0700

    [~] Fix EyeChart
    - จัด dataSet สำหรับ PureChart

--- src/components/EyeChart.js
+++ src/components/EyeChart.js
@@ -1,88 +1,88 @@
-import React from 'react';
-import { ScrollView, Text } from 'react-native';
-import { SQLite } from 'expo';
-import { ORANGE } from '../config';
+import React from "react";
+import { ScrollView, Text } from "react-native";
+import { SQLite } from "expo";
+import { ORANGE } from "../config";
 
-const historydb = SQLite.openDatabase('history.db');
-const eyeDropdb = SQLite.openDatabase('eyedrop.db');
+const historydb = SQLite.openDatabase("history.db");
+const eyeDropdb = SQLite.openDatabase("eyedrop.db");
 
 class EyeChart extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            data: [],
-            eyeDrop: [],
-            dataChart: [],
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+      eyeDrop: [],
+      dataChart: []
+    };
+  }
 
-    componentDidMount() {
-        this.historyData();
-        this.test();
-    }
+  componentDidMount() {
+    this.historyData();
+    this.test();
+  }
 
-    historyData() {
-        historydb.transaction(tx => {
-            tx.executeSql('select * from items where patientID = 1', [], (_, { rows: { _array } }) => {
-                if (_array.length > 0) {
-                    _array.forEach(eachEyeDrop => {
-                        this.getEyeDrops(eachEyeDrop.eyeDropID);
-                    });
-                    this.setState({
-                        data: _array
-                    });
-                }
+  historyData() {
+    historydb.transaction(tx => {
+      tx.executeSql(
+        "select * from items where patientID = 1",
+        [],
+        (_, { rows: { _array } }) => {
+          if (_array.length > 0) {
+            _array.forEach(eachEyeDrop => {
+              this.getEyeDrops(eachEyeDrop.eyeDropID);
             });
+            this.setState({
+              data: _array
+            });
+          }
+        }
+      );
+    });
+  }
+
+  getEyeDrops(ID) {
+    this.setState(
+      this.state.eyeDrop.includes(ID)
+        ? null
+        : { eyeDrop: this.state.eyeDrop.concat(ID) }
+    );
+  }
+
+  test() {
+    const { data, eyeDrop, dataSet } = this.state;
+    let result = [];
+    if (eyeDrop && data) {
+      eyeDrop.forEach(eachEyeDrop => {
+        result = [];
+        data.forEach(eachEle => {
+          if (eachEyeDrop == eachEle.eyeDropID) {
+            result.push({
+              x: eachEle.date,
+              y: parseFloat(eachEle.time)
+            });
+          }
         });
-    }
-
-    getEyeDrops(ID) {
-        this.setState(this.state.eyeDrop.includes(ID) ? null : { eyeDrop: this.state.eyeDrop.concat(ID) });
-    }
-
-    test() {
-        const { data, eyeDrop } = this.state
-        
-        // if(eyeDrop && data){
-        //     eyeDrop.forEach(eachEyeDrop => {
-        //         data.forEach(eachEle => {
-        //             if(eachEyeDrop==eachEle){
-
-        //             }
-        //         });
-        //     });
-        // }
-
-        const result = data.reduce((arr, item) => {
-            arr.push({
-                x: item.date,
-                y: parseInt(item.time),
-            });
-            return arr;
-        }, []);
-
-        console.log("result",result);
-        return [
+        if (result) {
+          dataSet[parseInt(eachEyeDrop)] = [
             {
-                data: result,
-                color: ORANGE
-            },
-        ];
+              data: result,
+              color: ORANGE
+            }
+          ];
+        }
+      });
     }
+  }
 
-    render() {
-        // console.log("data")
-        // console.log(this.state.data);
-        // console.log('--------------------------------------------');
-        // console.log("eyedrop",this.state.eyeDrop);
-        // console.log('--------------------------------------------');
-        // console.log(result,this.state.test())
-        return (
-            <ScrollView>
-                <Text>eiei</Text>
-            </ScrollView>
-        );
-    }
+  render() {
+    console.log("dataSet", dataSet);
+    console.log("--------------------------------------------");
+    return (
+      <ScrollView>
+        <Text>eiei</Text>
+      </ScrollView>
+    );
+  }
 }
 
 export default EyeChart;
